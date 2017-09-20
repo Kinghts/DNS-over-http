3319f39e7ef44813c042ff5b3a04e0ef0e0f4384
hh <hh@test.com>
2017-09-20 13:38:31 +0800

    使用map保存缓存

--- modules/cache.js
+++ modules/cache.js
@@ -2,10 +2,12 @@ const fs = require('fs')
 const path = require('path')
 const cacheDir = './cache/'
 Date.prototype.Format = require('./dateFormat').format
+const config = require('../config/server.config')
 
 function Cache () {
+
   this.records = {
-    'A': null
+    'A': null // Map
   }
 }
 
@@ -13,12 +15,24 @@ Cache.prototype.readCacheFile = function () {
   let promises = []
   for (let type in this.records) {
     promises.push(new Promise((resolve, reject) => {
-      fs.readFile(cacheDir + type + '.json', (err, data) => {
-        if (err) {
-          reject()
+      let filePath = cacheDir + type + '.json'
+      fs.exists(filePath, (exists) => {
+        if (exists) {
+          fs.readFile(filePath, (err, data) => {
+            if (err) {
+              reject(err)
+            }
+            let fileString = data.toString('utf-8')
+            if (fileString.match(/^\s*$/g)) { // 文件内容为空
+              this.records[type] = new Map()
+            }
+            this.records[type] = new Map(JSON.parse(fileString))
+            resolve()
+          })
+        } else {
+          this.records[type] = new Map()
+          resolve()
         }
-        this.records[type] = JSON.parse(data.toString('utf-8'))
-        resolve()
       })
     }))
   }
@@ -29,7 +43,7 @@ Cache.prototype.writeCacheToFile = function () {
   let promises = []
   for (let type in this.records) {
     promises.push(new Promise((resolve, reject) => {
-      fs.writeFile(cacheDir + type + '.json', JSON.stringify(this.records[type]), (err) => {
+      fs.writeFile(cacheDir + type + '.json', JSON.stringify([...this.records[type]]), (err) => {
         if (err) {
           reject()
         }
@@ -41,13 +55,24 @@ Cache.prototype.writeCacheToFile = function () {
 }
 
 Cache.prototype.getResult = function (type, domain) {
-  return this.records[type][domain]
+  let record = this.records[type].get(domain)
+  if (record) {
+    if ((new Date() - new Date(record.updateTime)) > config.catchControl.time) {
+      this.records[type].delete(domain)
+      return
+    }
+  }
+  return record
 }
 
 Cache.prototype.updateCache = function (type, domain, result) {
-  this.records[type][domain] = {
+  let records = this.records[type]
+  records.set(domain, {
     'result': result,
     'updateTime': new Date().Format('yyyy-MM-dd hh:mm:ss')
+  })
+  if (records.size > config.catchControl.maxNumber) {
+    records.delete(records.keys().next().value)
   }
 }
 
