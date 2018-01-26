9de8c832b242f9e728bde8b23cf276ab72e135b2
ss <ss@mail.com>
2018-01-26 11:35:59 +0800

    移除本地配置

--- config/block.config.js
+++ config/block.config.js
@@ -7,38 +7,6 @@ module.exports = {
     {
       url: 'test.com', // 要屏蔽子域名时，url请输入主域名，如test.com
       blockAll: true // 为true时会屏蔽所有子域名,如game.test.com
-    },
-    {
-      url: 'wps.cn',
-      blockAll: true
-    },
-    {
-      url: 'wpscdn.cn',
-      blockAll: true
-    },
-    {
-      url: 'ksosoft.com',
-      blockAll: true
-    },
-    {
-      url: 'nsclick.baidu.com',
-      blockAll: true
-    },
-    {
-      url: 'eclick.baidu.com',
-      blockAll: true
-    },
-    {
-      url: 'hm.baidu.com',
-      blockAll: true
-    },
-    {
-      url: 'bdimg.share.baidu.com',
-      blockAll: true
-    },
-    {
-      url: 'pos.baidu.com',
-      blockAll: true
-    },
+    }
   ]
 }
--- config/hosts.config.js
+++ config/hosts.config.js
@@ -4,21 +4,5 @@
 module.exports = [
   [
     'www.test.com', '127.0.0.1'
-    
-  ],
-  [
-    'github-cloud.s3.amazonaws.com','52.216.80.48'
-  ],
-  [
-    'github-com.s3.amazonaws.com', '54.231.40.3'
-  ],
-  [
-    'github-production-release-asset-2e65be.s3.amazonaws.com', '52.216.20.171'
-  ],
-  [
-    'github-production-user-asset-6210df.s3.amazonaws.com', '52.216.228.168'
-  ],
-  [
-    'maps.googleapis.com', '172.217.6.127'
   ]
 ]
